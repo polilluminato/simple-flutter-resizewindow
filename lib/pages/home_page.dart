e56f4be524c54142060c6ab21290c602a79602d6
Alberto Bonacina <bonacina.alberto@gmail.com>
2022-12-16 00:15:59 +0100

    feat: Update widgets and methods with the info from WindowSizeEnum

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,40 +9,24 @@ import 'package:window_manager/window_manager.dart';
 class HomePage extends ConsumerWidget {
   HomePage({Key? key}) : super(key: key);
 
-  void _changeWindowSize(WindowSizeEnum windowSize) {
-    switch (windowSize) {
-      case WindowSizeEnum.mobilePortrait:
-        //Display 2400x1080, Pixel 7
-        windowManager.setSize(const Size(400, 890));
-        break;
-      case WindowSizeEnum.mobileLandscape:
-        windowManager.setSize(const Size(890, 400));
-        break;
-      case WindowSizeEnum.tabletPortrait:
-        windowManager.setSize(const Size(607, 874));
-        break;
-      case WindowSizeEnum.tabletLandscape:
-        windowManager.setSize(const Size(874, 607));
-        break;
-      case WindowSizeEnum.desktop:
-        windowManager.setSize(const Size(1440, 900));
-        break;
-      default:
-        break;
-    }
-  }
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     WindowSize windowSize = ref.watch(windowSizeProvider);
     final ThemeData themeData = Theme.of(context);
-    final TextTheme textTheme = themeData.textTheme;
 
     void actionChangeWindowSize(WindowSizeEnum windowSizeEnum) {
-      _changeWindowSize(windowSizeEnum);
+      windowManager.setSize(
+        Size(
+          windowSizeEnum.width,
+          windowSizeEnum.height,
+        ),
+      );
+
       ref.read(windowSizeProvider.notifier).changeSize(
             WindowSize(
-                width: windowSizeEnum.width, height: windowSizeEnum.height),
+              width: windowSizeEnum.width,
+              height: windowSizeEnum.height,
+            ),
           );
     }
 
@@ -63,30 +47,12 @@ class HomePage extends ConsumerWidget {
             ),
             itemBuilder: (BuildContext context) {
               return [
-                PopupMenuItem(
-                  onTap: () =>
-                      actionChangeWindowSize(WindowSizeEnum.mobilePortrait),
-                  child: const Text('Mobile Portrait'),
-                ),
-                PopupMenuItem(
-                  onTap: () =>
-                      actionChangeWindowSize(WindowSizeEnum.mobileLandscape),
-                  child: const Text('Mobile Landscape'),
-                ),
-                PopupMenuItem(
-                  onTap: () =>
-                      actionChangeWindowSize(WindowSizeEnum.tabletPortrait),
-                  child: const Text('Tablet Portrait'),
-                ),
-                PopupMenuItem(
-                  onTap: () =>
-                      actionChangeWindowSize(WindowSizeEnum.tabletLandscape),
-                  child: const Text('Tablet Landscape'),
-                ),
-                PopupMenuItem(
-                  onTap: () => actionChangeWindowSize(WindowSizeEnum.desktop),
-                  child: const Text('Desktop'),
-                ),
+                for (WindowSizeEnum singleWindowSizeEnum
+                    in WindowSizeEnum.values)
+                  PopupMenuItem(
+                    onTap: () => actionChangeWindowSize(singleWindowSizeEnum),
+                    child: Text(singleWindowSizeEnum.description),
+                  )
               ];
             },
           )
@@ -96,7 +62,7 @@ class HomePage extends ConsumerWidget {
         child: Text(
           "Window Size\n${windowSize.width.toInt()}x${windowSize.height.toInt()}",
           textAlign: TextAlign.center,
-          style: textTheme.headline4,
+          style: themeData.textTheme.headline4,
         ),
       ),
     );

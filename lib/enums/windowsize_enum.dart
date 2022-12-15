10507b9f7fd18eb147176f9d4c1aa9881192db27
Alberto Bonacina <bonacina.alberto@gmail.com>
2022-12-16 00:15:10 +0100

    feat: Add new property to the WindowSizeEnum

--- lib/enums/windowsize_enum.dart
+++ lib/enums/windowsize_enum.dart
@@ -1,13 +1,14 @@
 // ignore_for_file: file_names
 
 enum WindowSizeEnum {
-  mobilePortrait(400, 890),
-  mobileLandscape(890, 400),
-  tabletPortrait(607, 874),
-  tabletLandscape(874, 607),
-  desktop(1440, 900);
+  mobilePortrait(400, 890, "Mobile Portrait"),
+  mobileLandscape(890, 400, "Mobile Landscape"),
+  tabletPortrait(607, 874, "Tablet Portrait"),
+  tabletLandscape(874, 607, "Tablet Landscape"),
+  desktop(1440, 900, "Desktop");
 
-  const WindowSizeEnum(this.width, this.height);
+  const WindowSizeEnum(this.width, this.height, this.description);
   final double width;
   final double height;
+  final String description;
 }
